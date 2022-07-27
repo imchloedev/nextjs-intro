952571161b5301e9b0c3d1b088abfae81548897b
Eunji Kang <etothej@naver.com>
2022-07-27 17:04:23 +0900

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
+// const API_KEY = "09cd6d519eaa550f9712a7241ec0b2b4";
+
+// 환경 변수로 감추기
+
+const API_KEY = process.env.API_KEY;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects () {
+    return [
+      {
+        source: '/contact',
+        destination: '/form',
+        permanent: false,
+      }
+    ]
+  },
+
+  async rewrites () {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,63 @@
-
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
-import styles from "../styles/Home.module.css";
-
-
-
-const API_KEY = "09cd6d519eaa550f9712a7241ec0b2b4";
-
 
 export default function Home() {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
-    .then((res) => {
-      return res.json();
+    fetch("api/movies", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
-    .then ((data) => {
-      const { results } = data;
-      // const results = data.results;
-      console.log(results);
-      setMovies(results); 
-    })
-  }, [])
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const { results } = data;
+        // const results = data.results;
+        console.log(results);
+        setMovies(results);
+      });
+  }, []);
 
   return (
-    <>
+    <div className="container">
       <Seo title="Home" />
-      <div>Hello</div>
       {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {movies?.map((movie) => (
         <div key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+          <div className="movie" key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <h4>{movie.original_title}</h4>
+          </div>
         </div>
       ))}
-    </>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
 }
